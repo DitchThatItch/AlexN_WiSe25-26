ed7ec91903315e5c1f2996c9ce0bcfbf5a25c0ad
DitchThatItch <53043078+DitchThatItch@users.noreply.github.com>
2025-11-26 15:36:13 +0100

    Tweaks
    Removed unnecessary print in recognize_posture
    Tweaked angle_interpolation variable t_now for better integration into server_agent

---
+++ kinematics/angle_interpolation.py
@@ -0,0 +1,88 @@
+'''In this exercise you need to implement an angle interploation function which makes NAO executes keyframe motion
+
+* Tasks:
+    1. complete the code in `AngleInterpolationAgent.angle_interpolation`,
+       you are free to use splines interploation or Bezier interploation,
+       but the keyframes provided are for Bezier curves, you can simply ignore some data for splines interploation,
+       please refer data format below for details.
+    2. try different keyframes from `keyframes` folder
+
+* Keyframe data format:
+    keyframe := (names, times, keys)
+    names := [str, ...]  # list of joint names
+    times := [[float, float, ...], [float, float, ...], ...]
+    # times is a matrix of floats: Each line corresponding to a joint, and column element to a key.
+    keys := [[float, [int, float, float], [int, float, float]], ...]
+    # keys is a list of angles in radians or an array of arrays each containing [float angle, Handle1, Handle2],
+    # where Handle is [int InterpolationType, float dTime, float dAngle] describing the handle offsets relative
+    # to the angle and time of the point. The first Bezier param describes the handle that controls the curve
+    # preceding the point, the second describes the curve following the point.
+'''
+
+
+from pid import PIDAgent
+from keyframes import leftBackToStand
+
+
+class AngleInterpolationAgent(PIDAgent):
+    def __init__(self, simspark_ip='localhost',
+                 simspark_port=3100,
+                 teamname='DAInamite',
+                 player_id=0,
+                 sync_mode=True):
+        super(AngleInterpolationAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
+        self.keyframes = ([], [], [])
+
+    def think(self, perception):
+        target_joints = self.angle_interpolation(self.keyframes, perception)
+        if'LHipYawPitch' in target_joints: # Some keyframes dont have LHipYawPitch!
+            target_joints['RHipYawPitch'] = target_joints['LHipYawPitch'] # copy missing joint in keyframes
+        self.target_joints.update(target_joints)
+        return super(AngleInterpolationAgent, self).think(perception)
+
+    def angle_interpolation(self, keyframes, perception):
+        target_joints = {}
+        # YOUR CODE HERE
+        if not hasattr(self, 'motion_start_time'):
+            self.motion_start_time = perception.time
+
+        self.t_now = perception.time - self.motion_start_time
+        names = keyframes[0]
+
+        for j in range(len(names)): 
+
+            times = keyframes[1][j]
+            keys = keyframes[2][j]
+
+            for i in range(len(times) - 1):
+                if times[i] <= self.t_now <= times[i+1]:
+                    break
+            else:
+                if self.t_now < times[0]:
+                    i = 0
+                else:
+                    i = len(times) - 2
+            
+            ang_start = keys[i][0]
+            ang_end = keys[i+1][0]
+            handle1 = keys[i][2]
+            handle2 = keys[i+1][1]
+
+            P0 = ang_start
+            P1 = ang_start + handle2[2]
+            P2 = ang_end + handle1[2]
+            P3 = ang_end
+
+            t0, t1 = times[i], times[i+1]
+            u = (self.t_now - t0) / (t1 - t0)
+            u = max(0.0, min(1.0, u))
+
+            angle = (1-u)**3*P0 + 3*(1-u)**2*u*P1 + 3*(1-u)*u**2*P2 + u**3*P3
+            target_joints[names[j]] = angle
+
+        return target_joints
+
+if __name__ == '__main__':
+    agent = AngleInterpolationAgent()
+    agent.keyframes = leftBackToStand() # CHANGE DIFFERENT KEYFRAMES
+    agent.run()
---
+++ kinematics/recognize_posture.py
@@ -0,0 +1,47 @@
+'''In this exercise you need to use the learned classifier to recognize current posture of robot
+
+* Tasks:
+    1. load learned classifier in `PostureRecognitionAgent.__init__`
+    2. recognize current posture in `PostureRecognitionAgent.recognize_posture`
+
+* Hints:
+    Let the robot execute different keyframes, and recognize these postures.
+
+'''
+
+
+from angle_interpolation import AngleInterpolationAgent
+from keyframes import leftBackToStand
+import pickle
+
+class PostureRecognitionAgent(AngleInterpolationAgent):
+    def __init__(self, simspark_ip='localhost',
+                 simspark_port=3100,
+                 teamname='DAInamite',
+                 player_id=0,
+                 sync_mode=True):
+        super(PostureRecognitionAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
+        self.posture = 'unknown'
+        with open(r"C:\Users\Alex\Desktop\RoboCup\AlexN_WiSe25-26-master\joint_control\robot_pose.pkl", "rb") as clf:
+            self.posture_classifier = pickle.load(clf)  # LOAD YOUR CLASSIFIER
+
+    def think(self, perception):
+        self.posture = self.recognize_posture(perception)
+        return super(PostureRecognitionAgent, self).think(perception)
+
+    def recognize_posture(self, perception):
+        posture = 'unknown'
+        # YOUR CODE HERE
+        joint_list = ['LHipYawPitch', 'LHipRoll', 'LHipPitch', 'LKneePitch', 
+        'RHipYawPitch', 'RHipRoll', 'RHipPitch', 'RKneePitch']
+        joint_angles = [self.target_joints[j] for j in joint_list]
+        angles = [perception.imu[0], perception.imu[1]]
+        features = joint_angles + angles
+
+        posture = self.posture_classifier.predict([features])[0]
+        return posture
+
+if __name__ == '__main__':
+    agent = PostureRecognitionAgent()
+    agent.keyframes = leftBackToStand()  # CHANGE DIFFERENT KEYFRAMES
+    agent.run()
